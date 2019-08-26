fb53c2b2ae839b651b6f6c908a474d60c386a1ed
Yves <yuhui.zhi01@gmail.com>
2019-08-26 17:28:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
+set -e
+
 hash gcc 2>/dev/null || { echo >&2 "gcc is not installed.  Aborting."; exit 1; }
 hash cmake 2>/dev/null || { echo >&2 "cmake is not installed.  Aborting."; exit 1; }
 hash make 2>/dev/null || { echo >&2 "make is not installed.  Aborting."; exit 1; }
