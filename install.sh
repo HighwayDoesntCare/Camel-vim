a0b2f52c188557241a6dd9914f20feccff314056
Yves <yuhui.zhi01@gmail.com>
2020-03-31 17:53:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ grep -q vimc ~/.bashrc || cat vimc >> ~/.bashrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cd ~/.vim/bundle && git clone https://github.com/Valloric/YouCompleteMe.git
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 ./install_YCM.sh
 
 vim -E -c PluginInstall -c q -c q
