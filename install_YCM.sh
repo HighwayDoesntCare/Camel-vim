f30253f23cd7c768ca36c9a3800c918c8df1c4ca
Yves <yuhui.zhi01@gmail.com>
2020-04-17 14:49:04 +0800

    Update install_YCM.sh

--- install_YCM.sh
+++ install_YCM.sh
@@ -6,4 +6,4 @@ cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 #cd ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/src/golang.org/x/ && git clone https://github.com/golang/tools.git
 #cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 
-~/.vim/bundle/YouCompleteMe/install.py --all
+~/.vim/bundle/YouCompleteMe/install.py --clang-completer
