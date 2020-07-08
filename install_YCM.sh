3f14ba54392fe3075dc8495583080790ae8d4b67
Yves <yuhui.zhi01@gmail.com>
2020-07-08 15:59:13 +0800

    Update install_YCM.sh

--- install_YCM.sh
+++ install_YCM.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+git config --global url."https://".insteadOf git://
 cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive
 #rm -rf ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/cregex
 #cd ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/src/golang.org/x/ && git clone https://github.com/golang/tools.git
